38a9936d37e535536b41f71934434b799133ff59
JOAO <joaocmspgomes@gmail.com>
2025-05-11 22:22:43 +0100

    bad file descriptor on the read end... wc: 'standard input': Bad file descriptor. WWHYUYYYY????

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -1,121 +1,67 @@
 #include "ft_pipes.h"
 
 // function to create pipes for the commands
-int make_pipes(t_cmd *cmd, t_shell *shell)
+int	make_pipes(t_cmd *cmd, t_shell *shell)
 {
-    if (!cmd || !cmd->next || shell->is_pipe == false)
-        return (0);
-    
-    t_cmd *tmp;
-
-    tmp = cmd;
-
-    while (tmp && tmp->next)
-    {
-        if (pipe(tmp->fd) == -1)
-        {
-            ft_printf_fd(STDERR_FILENO, "Error: pipe failed\n");
-            shell->exit_value = 1;
-            return (1);
-        }
-        tmp = tmp->next;
-    }
-    return (0);
+	if (!cmd || shell->is_pipe == false)
+		return (0);
+	
+	t_cmd	*tmp;
+	
+	tmp = cmd;
+	printf("Creating pipes... %p \n", cmd->prev);
+	while (tmp->next)
+	{
+		if (pipe(tmp->fd) == -1)
+		{
+			ft_printf_fd(STDERR_FILENO, "Error: pipe failed\n");
+			shell->exit_value = 1;
+			return (1);
+		}
+		//ft_printf_fd(2, "READ: %d\n", tmp->fd[0]);
+		//ft_printf_fd(2, "WRITE: %d\n", tmp->fd[1]);
+		tmp = tmp->next;
+	}
+	return (0);
 }
-
-
-
-
-
-
 
 // close pipes
 void	close_pipes(t_cmd *cmd)
 {
-    if (!cmd || !cmd->next)
-        return;
+	t_cmd	*tmp;
 
-    t_cmd *tmp1;
-    t_cmd *tmp2;
-
-    tmp1 = cmd;
-    tmp2 = cmd->next;
-    while (tmp2)
-    {
-        if (tmp1->fd[0] != -1)
-            close(tmp1->fd[0]);
-        if (tmp1->fd[1] != -1)
-            close(tmp1->fd[1]);
-        tmp1 = tmp2;
-        tmp2 = tmp2->next;
-    }
+	if (!cmd || !cmd->next)
+		return ;
+	tmp = cmd;
+	while (tmp && tmp->next)
+	{
+		close(tmp->fd[0]);
+		close(tmp->fd[1]);
+		tmp = tmp->next;
+	}
 }
 
 // function to manage pipes
-void manage_pipes(t_cmd *cmd, t_shell *shell)
+void	manage_pipes(t_cmd *cmd, t_shell *shell)
 {
-    if (!cmd || !cmd->next || shell->is_pipe == false)
-        return;
+	if (!cmd || shell->is_pipe == false)
+		return ;
 
-    t_cmd *tmp;
+	t_cmd	*tmp;
+	tmp = cmd;
 
-    tmp = cmd;
-	if (cmd->prev)  // if there's a previous command, read from it
+
+	if (cmd->prev)
+	{
 		dup2(cmd->prev->fd[0], STDIN_FILENO);
-	if (cmd->next)  // if there's a next command, write to it
+		close(cmd->prev->fd[0]);
+		close(cmd->prev->fd[1]);
+	}
+	if (cmd->next)
+	{
 		dup2(cmd->fd[1], STDOUT_FILENO);
-}
-
-
-/*
-
-
-
-
-// close the correct pipe
-void	close_correct_pipe(t_pipe *pipex, int cmd_index)
-{
-	if (cmd_index == 0)
-		close(pipex->cmds[cmd_index]->fd[1]);
-	else if (cmd_index == pipex->n_cmds - 1)
-		close(pipex->cmds[cmd_index - 1]->fd[0]);
-	else
-		close_pipe(pipex, cmd_index);
-}
-
-void	dup_handles(t_pipe *pipex, int cmd_index)
-{
-	if (cmd_index == 0)
-	{
-		close(pipex->cmds[cmd_index]->fd[0]);
-		dup2(pipex->infile, STDIN_FILENO);
-		close(pipex->infile);
-		dup2(pipex->cmds[cmd_index]->fd[1], STDOUT_FILENO);
-		close(pipex->cmds[cmd_index]->fd[1]);
+		close(cmd->fd[1]);
 	}
-	else if (cmd_index == pipex->n_cmds - 1)
-	{
-		close(pipex->cmds[cmd_index - 1]->fd[1]);
-		dup2(pipex->outfile, STDOUT_FILENO);
-		close(pipex->outfile);
-		dup2(pipex->cmds[cmd_index - 1]->fd[0], STDIN_FILENO);
-		close(pipex->cmds[cmd_index - 1]->fd[0]);
-	}
-	else
-	{
-		close(pipex->cmds[cmd_index - 1]->fd[1]);
-		close(pipex->cmds[cmd_index]->fd[0]);
-		dup2(pipex->cmds[cmd_index - 1]->fd[0], STDIN_FILENO);
-		close(pipex->cmds[cmd_index - 1]->fd[0]);
-		dup2(pipex->cmds[cmd_index]->fd[1], STDOUT_FILENO);
-		close(pipex->cmds[cmd_index]->fd[1]);
-	}
+	if (!cmd->next)
+		close (cmd->fd[0]);
 }
-
-
-
-
-
-
-
-*/
--- src/run/run_cmds.c
+++ src/run/run_cmds.c
@@ -1,170 +1,41 @@
 #include "ft_run.h"
 
-/* static void	pipe_builtin(t_cmd *cmd, t_shell *shell);
-
-void	run_commands(t_shell *shell)
-{
-	t_cmd	*tmp;
-
-	if (!shell->cmds)
-		return ;
-	tmp = shell->cmds;
-	while (tmp)
-	{
-		if (tmp->fd_struct)
-			setup_redirections(tmp, shell);
-		tmp = tmp->next;
-	}
-	tmp = shell->cmds;
-	if (shell->is_pipe)
-		make_pipes(tmp, shell);
-	while (tmp)
-	{
-		if (tmp->is_valid)
-			pipe_builtin(tmp, shell);
-		tmp = tmp->next;
-	}
-} */
-/* 
 void	pipe_builtin(t_cmd *cmd, t_shell *shell)
 {
-	t_cmd	*tmp;
-	t_cmd	*walker;
-	int		status;
-
-	status = 0;
-	tmp = cmd;
-	if (shell->is_pipe)
-	{
-		tmp->pid = fork();
-		if (tmp->pid == 0)
-		{
-			signal(SIGQUIT, SIG_DFL);
-			signal(SIGINT, SIG_DFL);
-			shell->is_child = true;
-			if (tmp->prev)
-			{
-				dup2(tmp->prev->fd[0], STDIN_FILENO);
-				close(tmp->prev->fd[0]);
-			}
-			if (tmp->next)
-			{
-				dup2(tmp->fd[1], STDOUT_FILENO);
-				close(tmp->fd[1]);
-			}
-			walker = shell->cmds;
-			while (walker)
-			{
-				if (walker->fd[0] != -1)
-					close(walker->fd[0]);
-				if (walker->fd[1] != -1)
-					close(walker->fd[1]);
-				walker = walker->next;
-			}
-			// --- EXECUTION ---
-			tmp->builtin_func(tmp, shell);
-			clean_exit(&shell);
-		}
-		else if (tmp->pid < 0)
-		{
-			ft_printf_fd(tmp->fd[1], "minishell: fork failed\n");
-			shell->exit_value = 1;
-		}
-		else
-		{
-			waitpid(tmp->pid, &status, 0);
-			if (WIFSIGNALED(status))
-			{
-				if (WTERMSIG(status) == SIGQUIT)
-					ft_putstr_fd("Quit (core dumped)\n", STDERR_FILENO);
-				shell->exit_value = 128 + WTERMSIG(status);
-			}
-			else if (WIFEXITED(status))
-				shell->exit_value = WEXITSTATUS(status);
-			if (tmp->prev && tmp->prev->fd[0] != -1)
-				close(tmp->prev->fd[0]);
-			if (tmp->fd[1] != -1)
-				close(tmp->fd[1]);
-		}
-	}
-	else
-		cmd->builtin_func(tmp, shell);
-} */
-
-void	pipe_builtin(t_cmd *cmd, t_shell *shell)
-{
-	t_cmd	*walker;
-
 	if (!shell->is_pipe)
 	{
 		cmd->builtin_func(cmd, shell);
-		return;
+		return ;
 	}
-
 	cmd->pid = fork();
 	if (cmd->pid == -1)
 	{
 		ft_putstr_fd("minishell: fork failed\n", STDERR_FILENO);
 		shell->exit_value = 1;
-		return;
+		return ;
 	}
-
 	if (cmd->pid == 0)
 	{
-		// --- CHILD ---
-		signal(SIGQUIT, SIG_DFL);
-		signal(SIGINT, SIG_DFL);
 		shell->is_child = true;
 
-		if (cmd->prev && cmd->prev->fd[0] != -1)
-		{
-			if (dup2(cmd->prev->fd[0], STDIN_FILENO) == -1)
-			{
-				perror("dup2 in");
-				clean_exit(&shell);
-			}
-		}
-
-		if (cmd->next && cmd->fd[1] != -1)
-		{
-			if (dup2(cmd->fd[1], STDOUT_FILENO) == -1)
-			{
-				perror("dup2 out");
-				clean_exit(&shell);
-			}
-		}
-
-		// Close all pipe fds to avoid leaks or zombie fds
-		for (walker = shell->cmds; walker; walker = walker->next)
-		{
-			if (walker->fd[0] != -1)
-				close(walker->fd[0]);
-			if (walker->fd[1] != -1)
-				close(walker->fd[1]);
-		}
-
+		manage_pipes(cmd, shell);
+		//close_pipes(cmd);
+		
 		// Builtin execution
 		cmd->builtin_func(cmd, shell);
 		clean_exit(&shell);
 	}
+	// In parent process, after forking
 	else
 	{
-		// --- PARENT ---
-
-		// Close parent's copies of pipe ends
-		if (cmd->prev && cmd->prev->fd[0] != -1)
+		if (cmd->prev )
 		{
 			close(cmd->prev->fd[0]);
-			cmd->prev->fd[0] = -1;
 		}
-
-		if (cmd->fd[1] != -1)
+		if (cmd->next)
 		{
 			close(cmd->fd[1]);
-			cmd->fd[1] = -1;
 		}
-
-		// DO NOT wait here; let run_commands() wait for all later
 	}
 }
 
@@ -174,21 +45,21 @@ void	run_commands(t_shell *shell)
 
 	if (!shell->cmds)
 		return ;
-
-	// Setup redirs
-	tmp = shell->cmds;
-	while (tmp)
-	{
-		if (tmp->fd_struct)
-			setup_redirections(tmp, shell);
-		tmp = tmp->next;
-	}
-
-	// Setup pipes
+	
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
+	
+		// Setup pipes
 	tmp = shell->cmds;
 	if (shell->is_pipe)
-		make_pipes(tmp, shell);
+		if (make_pipes(tmp, shell) == 1)
+			return ;
 
+	while (tmp)
+	{
+		setup_redirections(tmp, shell);
+		tmp = tmp->next;
+	}
 	// Launch commands
 	tmp = shell->cmds;
 	while (tmp)
@@ -197,7 +68,6 @@ void	run_commands(t_shell *shell)
 			pipe_builtin(tmp, shell);
 		tmp = tmp->next;
 	}
-
 	// Wait for all children
 	tmp = shell->cmds;
 	while (tmp)
@@ -206,5 +76,7 @@ void	run_commands(t_shell *shell)
 			waitpid(tmp->pid, NULL, 0);
 		tmp = tmp->next;
 	}
+	// Close pipes
+	tmp = shell->cmds;
+	close_pipes(tmp);
 }
-
